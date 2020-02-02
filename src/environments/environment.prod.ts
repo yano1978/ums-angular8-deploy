af6abc1fdf8954956b1bdd0c7d9107818863957d
ernestoianuario <yano1978@gmail.com>
2020-02-02 12:37:24 +0100

    Changing URL for the Heroku App

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  APIRUL : 'http://glacial-stream-86487.herokuapp.com/users',
+  APIRUL : 'https://ums-angular-crud.herokuapp.com/users',
   APIAUTH : 'http://glacial-stream-86487.herokuapp.com/api/auth'
 };
