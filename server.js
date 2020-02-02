2d95701f7c643b2ba1f5f7a89fc67d32b7529535
ernestoianuario <yano1978@gmail.com>
2020-02-02 14:10:46 +0100

    Name app change

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/ums'));
+app.use(express.static(__dirname + '/dist/ums-angular-crud'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/ums/index.html'));
+res.sendFile(path.join(__dirname+'/dist/ums-angular-crud/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
