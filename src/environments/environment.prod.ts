a3122dbef14f029c5333aadba7d81813a5072fcf
ernestoianuario <yano1978@gmail.com>
2020-02-02 17:08:47 +0100

    SSL test to solve mixed req

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  APIRUL : 'http://glacial-stream-86487.herokuapp.com/users',
+  APIRUL : 'https://ums-angular.herokuapp.com/users',
   APIAUTH : 'http://glacial-stream-86487.herokuapp.com/api/auth'
 };
