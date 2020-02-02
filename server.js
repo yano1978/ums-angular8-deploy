0577d2e5cd37d0dac98b8c43ec4ec672e95aaf6e
ernestoianuario <yano1978@gmail.com>
2020-02-02 16:57:21 +0100

    Other fixings

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/UMS'));
 
-app.get('/users', function(req,res) {
+app.get('/*', function(req,res) {
     
 res.sendFile(path.join(__dirname+'/dist/UMS/index.html'));
 });
