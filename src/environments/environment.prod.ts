da6b56adf5887386d9bcb0732cbdf976898a27b0
ernestoianuario <yano1978@gmail.com>
2020-02-02 17:15:17 +0100

    Add SSL new test

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  APIRUL : 'https://ums-angular.herokuapp.com/users',
-  APIAUTH : 'http://glacial-stream-86487.herokuapp.com/api/auth'
+  APIRUL : 'https://glacial-stream-86487.herokuapp.com/users',
+  APIAUTH : 'https://glacial-stream-86487.herokuapp.com/api/auth'
 };
