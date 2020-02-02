f1d370efde49e934c5cada20bc038cdbebed3109
ernestoianuario <yano1978@gmail.com>
2020-02-02 13:00:58 +0100

    Add node server.js & fixing of the URL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  APIRUL : 'https://ums-angular-crud.herokuapp.com/',
+  APIRUL : 'https://ums-angular-crud.herokuapp.com/users',
   APIAUTH : 'http://glacial-stream-86487.herokuapp.com/api/auth'
 };
